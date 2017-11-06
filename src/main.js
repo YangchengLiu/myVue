dfcc1cccbfd3d120c4d2e4841e6f029555ada0d8
YangchengLiu <763363186@qq.com>
2017-11-06 14:31:33 +0800

    完成新闻列表，跳转新闻详情，加载数据评论

--- src/main.js
+++ src/main.js
@@ -4,10 +4,18 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 //1.2安装路由
 Vue.use(VueRouter)
+//导入格式化时间的插件
+import moment from 'moment'
+//定义全局的过滤器
+Vue.filter('dataFormat',function(dataStr,pattern="YYYY-MM-DD HH:mm:ss"){
+    return moment(dataStr).format(pattern)
+})
 //2.1 导入 vue-resource
 //2.2 安装 vue-resource
 import VueResource from 'vue-resource'
 Vue.use(VueResource)
+//设置请求的根路径
+Vue.http.options.root='http://vue.studyit.io'
 //导入 MUI 的样式
 import './lib/mui/css/mui.min.css'
 import './lib/mui/css/icons-extra.css'
@@ -16,10 +24,11 @@ import app from './App.vue'
 //1.3淡入自己的router.js路由模块
 import router from './router.js'
 //按需导入 Mint-UI中的组件
-import {Header,Swipe, SwipeItem} from 'mint-ui'
+import {Header,Swipe, SwipeItem,Button} from 'mint-ui'
 Vue.component(Header.name,Header)
-Vue.component(Swipe.name, Swipe);
+Vue.component(Swipe.name, Swipe)
 Vue.component(SwipeItem.name, SwipeItem)
+Vue.component(Button.name, Button)
 var vm = new Vue({
     el: '#app',
     render: c => c(app),
